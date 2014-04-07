7f19000ffc5351fdb69cffdb870c2e2e755d057a
dinhln <dinhln@bravebits.vn>
2014-04-07 16:51:19 +0700

    [DLN-SKILL] Add Post Status Filter

--- dln-skill/dln-form/includes/admin/class-dln-form-admin-post-type.php
+++ dln-skill/dln-form/includes/admin/class-dln-form-admin-post-type.php
@@ -16,46 +16,19 @@ class DLN_Form_Admin_PostType {
 	}
 	
 	function __construct() {
-		add_action( 'restrict_manage_posts', array( $this, 'company_status_filters' ) );
-		add_filter( 'parse_query', array( $this, 'company_status_parse_query' ) );
+		add_action( 'init', array( $this, 'dln_company_status_filters' ) );
 	}
 	
-	public function company_status_parse_query() {
-		global $pagenow;
-		$post_type = DLN_COMPANY_SLUG; // change HERE
-		
-		$q_vars = &$query->query_vars;
-		if ( $pagenow == 'edit.php' && isset($q_vars['post_type'] ) && $q_vars['post_type'] == $post_type && isset( $q_vars[ $taxonomy ] ) && is_numeric( $q_vars[ $taxonomy ] ) && $q_vars[ $taxonomy ] != 0 ) {
-			$term              = get_term_by( 'id', $q_vars[$taxonomy], $taxonomy );
-			$q_vars[$taxonomy] = $term->slug;
+	public function dln_company_status_filters() {
+		$types = self::get_company_status();
+		foreach ( $types as $name => $type ) {
+			register_post_status( $name, array(
+				'label'       => $type,
+				'private'     => true,
+				'_builtin'    => true, /* internal use only. */
+				'label_count' => _n_noop( $type . ' <span class="count">(%s)</span>', $type . ' <span class="count">(%s)</span>' ),
+			) );
 		}
-	}
-	
-	public function company_status_filters() {
-		global $typenow;
-		
-		if ( $typenow != DLN_COMPANY_SLUG )
-			return;
-		
-		// Type Select
-		?>
-		<select name="company_status" id="dropdown_company_status">
-			<option value=""><?php _e( 'Show all types', 'dln-skill' ); ?></option>
-			<?php
-				$types = self::get_company_status();
-
-				foreach ( $types as $name => $type ) {
-					echo '<option value="' . esc_attr( $name ) . '"';
-
-					if ( isset( $_GET['company_status'] ) )
-						selected( $name, $_GET['company_status'] );
-
-					echo '>' . esc_html__( $type, 'dln-skill' ) . '</option>';
-				}
-			?>
-		</select>
-		<?php
-		
 	}
 	
 	public static function get_company_status() {
