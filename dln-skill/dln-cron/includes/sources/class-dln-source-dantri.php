ce7bf2cd1b27d3f974b765bda2efb493666eabf4
dinhln <dinhln@bravebits.vn>
2014-04-16 16:47:16 +0700

    [DLN-CRON] Fixed crawl dantri

--- dln-skill/dln-cron/includes/sources/class-dln-source-dantri.php
+++ dln-skill/dln-cron/includes/sources/class-dln-source-dantri.php
@@ -4,6 +4,8 @@ if ( ! defined( 'WPINC' ) ) { die; }
 
 class DLN_Source_Dantri extends DLN_Source {
 	
+	public static $source_type = 'dan-tri';
+	
 	function __construct() {
 		
 	}
@@ -24,33 +26,32 @@ class DLN_Source_Dantri extends DLN_Source {
 				$id       = isset( $matches[1][0] ) ? $matches[1][0] : 0;
 				if ( ! in_array( $id, $arr_ids ) ) {
 					$arr_ids[] = $id;
+					$obj_item                = array();
+					$obj_item['link']        = trim( $link );
+					$obj_item['host_id']     = trim( $id );
+					$obj_item['site']        = self::$source_type;
+					$obj_item['is_crawl']    = '0';
+					$obj_item['time_create'] = date( 'Y-m-d H:i:s', time() );
+					$arr_objs[]              = $obj_item;
 				}
-				$obj_item                = array();
-				$obj_item['link']        = $link;
-				$obj_item['host_id']     = $id;
-				$obj_item['site']        = 'dantri';
-				$obj_item['is_crawl']    = '0';
-				$obj_item['time_create'] = date( 'Y-m-d H:i:s', time() );
-				$arr_objs[]              = $obj_item;
 			}
 			
 			// Check id exists in db
+			$current_time = date( 'Y-m-d H:i:s', time() );
 			$str_ids = ( count( $arr_ids ) > 0 ) ? implode( $arr_ids, ',' ) : '';
-			$sql     = "SELECT id FROM {$wpdb->prefix}dln_crawl_links WHERE id IN ({$str_ids})";
-			$results = $wpdb->get_row( $sql );
+			$sql     = "SELECT host_id FROM {$wpdb->prefix}dln_crawl_links WHERE site = '" . self::$source_type . "' AND host_id IN ({$str_ids})";
+			$results = $wpdb->get_col( $sql );
 			if ( $results ) {
 				foreach ( $results as $i => $item ) {
-					if ( isset( $item['id'] ) ) {
-						self::write_log( 'Duplicate <b>id</b> of crawl link in Database: ' . $item['id'] . '<br />' );
-						$idx = array_search( $item['id'], $arr_ids );
-						if ( $idx )
-							unset( $arr_ids[$idx] );
+					if ( $item ) {
+						$idx = array_search( $item, $arr_ids );
+						unset( $arr_ids[$idx] );
 					}
 				}
 			}
 			
 			// Insert link to db
-			if( $arr_objs ) {
+			if( count( $arr_objs ) && count( $arr_ids ) ) {
 				foreach ( $arr_objs as $i => $obj ) {
 					if ( in_array( $obj['host_id'], $arr_ids ) ) {
 						$int = $wpdb->insert(
@@ -58,9 +59,9 @@ class DLN_Source_Dantri extends DLN_Source {
 							$obj
 						);
 						if ( $int ) {
-							self::write_log( 'Completed insert <b>id</b> of crawl link in Database: ' . $obj['host_id'] . ' - ' . $obj['link'] . '<br />' );
+							self::write_log( 'Completed insert <b>id</b> of crawl link in Database: ' . $obj['host_id'] . ' - ' . $obj['link'] . '  at time ' . $current_time . '<br />' );
 						} else {
-							self::write_log( 'Error insert <b>id</b> of crawl link in Database: ' . $obj['host_id'] . '<br />' );
+							self::write_log( 'Error insert <b>id</b> of crawl link in Database: ' . $obj['host_id'] . ' at time ' . $current_time . '<br />' );
 						}
 					}
 				}
