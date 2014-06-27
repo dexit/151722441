a07716aa0707298de681a4be2e7be435daaf4a13
dinhln <dinhln@bravebits.vn>
2014-06-27 16:34:53 +0700

    [DLN-HOT-POINT] Update server

--- dln-hot-point/server/app.js
+++ dln-hot-point/server/app.js
@@ -2,10 +2,11 @@ var fs = require('fs'),
 	mongoose = require('mongoose'),
 	passport = require('passport'),
 	http = require('http'),
-	mongodbURI = 'mongodb://localhost/test1234', /* For example: mongodb://localhost/my-app-db */
-	facebookAppId = '1446113338979798',
-	facebookAppSecret = '0b996585ef99c01b4c486006a525e3d6',
-	dlnServerUrl = 'http://localhost:3000';
+	_ = require('underscore'),
+	mongodbURI = 'mongodb://localhost/test1234'; /* For example: mongodb://localhost/my-app-db */
+	//facebookAppId = '1446113338979798',
+	//facebookAppSecret = '0b996585ef99c01b4c486006a525e3d6',
+	//dlnServerUrl = 'http://localhost:3000';
 
 mongoose.connect(mongodbURI);
 
@@ -16,14 +17,15 @@ var models_path = __dirname + '/models';
 	 }
  });
 
-require('./config/passport')(passport, facebookAppId, facebookAppSecret);
+//require('./config/passport')(passport, facebookAppId, facebookAppSecret);
 
 var app = require('./config/express')(passport);
-
-require('./config/routes')(app, passport, facebookAppId, dlnServerUrl);
-
-http.createServer(app).listen(app.get('port'), function(){
+app.set('port', process.env.PORT || 3000);
+var server = app.listen(app.get('port'), function() {
 	console.log('Express server listening on port ' + app.get('port'));
 });
+
+var io = require('socket.io').listen(server);
+var socket_module = require('./config/socket')(io, _);
 
 exports = module.exports = app;
--- dln-hot-point/server/config/socket.js
+++ dln-hot-point/server/config/socket.js
@@ -1,12 +1,69 @@
-var usernames = {};
+var Room = require('room.js');
+var people = {};
+var rooms = {};
+var rooms_helper = require('rooms-helpers.js');
+var sockets = [];
+var chatHistory = {};
 
-module.exports = function (socket) {
-	socket.on('add-user', function(username) {
-		socket.username = username;
-		usernames[username] = username;
+function checkExistsRoom( roomID ) {
+	var exists = false;
+	_.find(rooms, function (key, value) {
+		if ( key.id.toLowerCase() == roomID.toLowerCase() ) {
+			return exists = true;
+		}
+	});
 
-		socket.emit('update-user-list', 'SERVER', 'you have connected!');
-		socket.broadcast.emit('update-user-list', 'SERVER', username + ' has connected!');
-		io.sockets.emit('update-users', usernames);
+	return exists;
+};
+
+function createRoom( roomID, room ) {
+	rooms[roomID] = room;
+
+}
+
+module.exports = function (io, _) {
+	io.sockets.on('connection', function (client) {
+
+		client.on('join-server', function (name, device, location_id) {
+			var exist = false;
+			var ownerRoomID = inRoomID = null;
+
+			checkExistsRoom(location_id);
+
+			if ( ! exists ) {
+				// Create new room
+				createRoom( location_id );
+			} else {
+
+			}
+
+			/*_.find(people, function (key, value) {
+				if ( key.name.toLowerCase() === name.toLowerCase() )
+					return exists = true;
+			});
+
+			if ( exists ) {
+				client.emit('user-exists', {msg: "The username already exists!"});
+			} else {
+				people[socket.id] = {"name" : name, "owns" : ownerRoomID, "inroom": inRoomID, "device": device};
+				client.emit("log-update", "You have connected to the server.");
+				io.sockets.emit("log-update", people[socket.id].name + " is online.")
+				sizePeople = _.size(people);
+				sizeRooms = _.size(rooms);
+				io.sockets.emit("update-people", {people: people, count: sizePeople});
+				client.emit("room-list", {rooms: rooms, count: sizeRooms});
+				client.emit("joined"); //extra emit for GeoLocation
+				sockets.push(socket);
+			}*/
+		});
+
+		client.on('add-user', function(username) {
+			client.username = username;
+			usernames[username] = username;
+
+			client.emit('update-user-list', 'SERVER', 'you have connected!');
+			client.broadcast.emit('update-user-list', 'SERVER', username + ' has connected!');
+			io.sockets.emit('update-users', usernames);
+		});
 	});
 }
