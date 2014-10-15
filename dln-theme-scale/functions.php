94598032e8bdbee7f52e0cbbfa0622594b198dd1
lenhatdinh@gmail.com <lenhatdinh@gmail.com>
2014-10-15 20:03:00 +0700

    minor

--- dln-theme-scale/functions.php
+++ dln-theme-scale/functions.php
@@ -11,10 +11,6 @@ function dln_theme_scripts() {
         wp_enqueue_script( 'dln-scale-noconflict-js', get_template_directory_uri() . '/assets/js/dln-noconflict.js', array( 'dln-scale-app-js' ), DLN_SCALE_VERSION, true );
         wp_enqueue_style( 'dln-scale-app-css', get_template_directory_uri() . '/assets/css/app.v1.css', null, DLN_SCALE_VERSION );
         wp_enqueue_style( 'dln-scale-style-css', get_template_directory_uri() . '/assets/css/dln-style.css', null, DLN_SCALE_VERSION );
-        
-        //wp_localize_script( 'dln-frontend-app-js', 'DLN_Vars', array( 
-        //    'root_url' => get_template_directory_uri() . '/assets/'
-        //) );
     }
 }
 add_action( 'wp_enqueue_scripts', 'dln_theme_scripts' );
