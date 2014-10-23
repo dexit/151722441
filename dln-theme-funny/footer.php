74335024afa2b177c32974acce533174a761b7d2
lenhatdinh@gmail.com <lenhatdinh@gmail.com>
2014-10-23 23:22:09 +0700

    add funny theme

---
+++ dln-theme-funny/footer.php
@@ -0,0 +1,39 @@
+			</div>
+		</section>
+		<!-- END Page content-->
+		<!-- START Page footer-->
+		<footer>&copy; 2014 - Wintermin</footer>
+		<!-- END Page Footer-->
+		<!-- END Main section-->
+	</div>
+	<script src="<?php echo get_template_directory_uri(); ?>/assets/3rd-party/jquery/jquery.min.js"></script>
+	<script src="<?php echo get_template_directory_uri(); ?>/assets/3rd-party/bootstrap/js/bootstrap.min.js"></script>
+	<!-- Plugins-->
+	<script src="<?php echo get_template_directory_uri(); ?>/assets/3rd-party/chosen/chosen.jquery.min.js"></script>
+	<script src="<?php echo get_template_directory_uri(); ?>/assets/3rd-party/slider/js/bootstrap-slider.js"></script>
+	<script src="<?php echo get_template_directory_uri(); ?>/assets/3rd-party/filestyle/bootstrap-filestyle.min.js"></script>
+	<!-- Animo-->
+	<!-- <script src="vendor/animo/animo.min.js"></script> -->
+	<!-- Sparklines-->
+	<!-- <script src="vendor/sparklines/jquery.sparkline.min.js"></script> -->
+	<!-- Slimscroll-->
+	<script src="<?php echo get_template_directory_uri(); ?>/assets/3rd-party/slimscroll/jquery.slimscroll.min.js"></script>
+	<!-- Store + JSON-->
+	<script src="<?php echo get_template_directory_uri(); ?>/assets/3rd-party/store/store%2bjson2.min.js"></script>
+	<!-- Classyloader-->
+	<!-- <script src="vendor/classyloader/js/jquery.classyloader.min.js"></script> -->
+	<!-- START Page Custom Script-->
+	<!--  Flot Charts-->
+	<!-- <script src="vendor/flot/jquery.flot.min.js"></script>
+	<script src="vendor/flot/jquery.flot.tooltip.min.js"></script>
+	<script src="vendor/flot/jquery.flot.resize.min.js"></script>
+	<script src="vendor/flot/jquery.flot.pie.min.js"></script>
+	<script src="vendor/flot/jquery.flot.time.min.js"></script>
+	<script src="vendor/flot/jquery.flot.categories.min.js"></script> -->
+	<!--[if lt IE 8]><script src="js/excanvas.min.js"></script><![endif]-->
+	<!-- END Page Custom Script-->
+	<!-- App Main-->
+	<script src="<?php echo get_template_directory_uri(); ?>/assets/js/app.js"></script>
+	<!-- END Scripts-->
+</body>
+</html>
