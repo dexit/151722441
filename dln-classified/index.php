e91abeedb5b92c737dfc657f6265714fd9f4c7f1
lenhatdinh@gmail.com <lenhatdinh@gmail.com>
2014-10-30 22:56:47 +0700

    add classified

---
+++ dln-classified/index.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: DLN Classified
+Plugin URI: http://www.osclass.org/
+Description: This plugin shows a Google Map on the location space of every item.
+Version: 2.1.6
+Author: Osclass & kingsult
+Author URI: http://www.osclass.org/
+Plugin update URI: http://www.osclass.org/files/plugins/google_maps/update.php
+*/
