1b3a9a617ce7d1d314b5f45d0089ec6d8babd90c
dinhln <dinhln@dinhln-Vostro-460>
2014-12-25 15:50:09 +0700

    fix map drag

--- dln-october/dlnlab/classified/assets/js/field-map.js
+++ dln-october/dlnlab/classified/assets/js/field-map.js
@@ -1,137 +1,129 @@
 (function ($) {
     "use strict";
 
-    var field_address = "dln_address";
-    var longval = "#clf_lat";
-    var latval = "#clf_long";
-    var mapId = 'dln_map_canvas';
-    var geocoder;
-    var map;
-    var marker;
-    var autocomplete;
-
-    function initialize() {
-        //MAP
-        var initialLat = $(latval).val();
-        var initialLong = $(longval).val();
-        if (initialLat == '') {
-            initialLat = "51.773071843208115";
-            initialLong = "-1.6568558468750325";
-        }
-        var latlng = new google.maps.LatLng(initialLat, initialLong);
+    var MapDrag = function () {
+        this.$lat     = $('input[name="latitude"]');
+        this.$long    = $('input[name="longitude"]');
+        this.$map     = $('#dln_map_canvas');
+        this.$address = $('#dln_address');
+        this.map_id          = document.getElementById('dln_map_canvas');
+        this.autocomplete_id = document.getElementById('dln_address');
+        this.lat_val      = (this.$lat.length && this.$lat.val()) ? this.$lat.val() : '21.0277644';
+        this.long_val     = (this.$long.length && this.$long.val()) ? this.$long.val() : '105.83415979999995';
+        this.geocoder     = null;
+        this.marker       = null;
+        this.map          = null;
+        this.autocomplete = null;
+        this.latlng       = new google.maps.LatLng(this.lat_val, this.long_val);
+        this.s_placeholder = 'Enter a city';
+        
+        this.initMap();
+        this.initMarker();
+        this.initAutocomplete();
+        this.initEvents();
+    }
+    
+    MapDrag.prototype.initMap = function () {
+        var self = this;
+        
         var options = {
             zoom: 16,
-            center: latlng,
+            center: this.latlng,
             mapTypeId: google.maps.MapTypeId.ROADMAP
         };
-
-        map = new google.maps.Map(document.getElementById(mapId), options);
-
-        geocoder = new google.maps.Geocoder();
-
-        marker = new google.maps.Marker({
-            map: map,
-            draggable: true,
-            position: latlng
-        });
-
-        google.maps.event.addListener(marker, "dragend", function (event) {
-            var point = marker.getPosition();
-            map.panTo(point);
-            loadAddressMarker();
-        });
-
+        this.map      = new google.maps.Map(self.map_id, options);
+        this.geocoder = new google.maps.Geocoder();
     }
-    ;
-
-    function loadAddressMarker() {
-        geocoder.geocode({'latLng': marker.getPosition()}, function (results, status) {
+    
+    MapDrag.prototype.initMarker = function () {
+        var self = this;
+        
+        this.marker = new google.maps.Marker({
+            map: self.map,
+            draggable: true,
+            position: self.latlng
+        });
+    }
+    
+    MapDrag.prototype.initAutocomplete = function () {
+        var self = this;
+        
+        this.autocomplete = new google.maps.places.Autocomplete(self.autocomplete_id, {
+            types: ['geocode']
+        });
+        
+        // Prevent enter key
+        /*self.$address.keypress(function (event) {
+            if (event.keyCode == 13) {
+                event.preventDefault();
+            }
+        });*/
+    }
+    
+    MapDrag.prototype.initEvents = function () {
+        var self = this;
+        
+        // For dragend marker
+        google.maps.event.addListener(self.marker, 'dragend', function (event) {
+           var point = self.marker.getPosition() ;
+           self.map.panTo(point);
+           self.loadAddress();
+        });
+        
+        //Add listener to marker for reverse geocoding
+        google.maps.event.addListener(self.marker, 'drag', function () {
+            self.loadAddress();
+        });
+        
+        // For autocomplete
+        google.maps.event.addListener(self.autocomplete, 'place_changed', function () {
+            // hide marker
+            self.marker.setVisible(false);
+            
+            var place = self.autocomplete.getPlace();
+            if (place.geometry) {
+                self.map.panTo(place.geometry.location);
+                self.map.setZoom(15);
+                
+                self.marker.setPosition(place.geometry.location);
+                self.marker.setVisible(true);
+                self.$lat.val(self.marker.getPosition().lat());
+                self.$long.val(self.marker.getPosition().lng());
+            } else {
+                self.$address.placeholder = self.s_placeholder;
+            }
+        });
+    }
+    
+    MapDrag.prototype.loadAddress = function () {
+        var self = this;
+        
+        this.geocoder.geocode({'latLng': self.marker.getPosition()}, function (results, status) {
             if (status == google.maps.GeocoderStatus.OK) {
                 if (results[0]) {
-                    $(latval).val(marker.getPosition().lat());
-                    $(longval).val(marker.getPosition().lng());
-                    $('#dln_address').val(results[0].formatted_address);
+                    self.$lat.val(self.marker.getPosition().lat());
+                    self.$long.val(self.marker.getPosition().lng());
+                    self.$address.val(results[0].formatted_address);
                 }
             }
         });
     }
 
-    function geolocate() {
-        if (autocomplete) {
+    MapDrag.prototype.geolocate = function () {
+        var self = this;
+        
+        if (this.autocomplete) {
             if (navigator.geolocation) {
                 navigator.geolocation.getCurrentPosition(function (position) {
                     var geolocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-                    autocomplete.setBounds(new google.maps.LatLngBounds(geolocation, geolocation));
+                    self.autocomplete.setBounds(new google.maps.LatLngBounds(geolocation, geolocation));
                 });
             }
         }
     }
 
     $(document).ready(function () {
-
-        initialize();
-
-        $(function () {
-            autocomplete = new google.maps.places.Autocomplete(document.getElementById(field_address), {types: ['geocode']});
-
-            google.maps.event.addListener(autocomplete, 'place_changed', onPlaceChanged);
-
-            function onPlaceChanged() {
-                marker.setVisible(false);
-                var place = autocomplete.getPlace();
-                if (place.geometry) {
-                    map.panTo(place.geometry.location);
-                    map.setZoom(15);
-                    marker.setPosition(place.geometry.location);
-                    marker.setVisible(true);
-                } else {
-                    document.getElementById(field_address).placeholder = 'Enter a city';
-                }
-
-            }
-
-            /*$(PostCodeid).autocomplete({
-             //This bit uses the geocoder to fetch address values
-             source: function (request, response) {
-             geocoder.geocode({ 'address': request.term }, function (results, status) {
-             response($.map(results, function (item) {
-             return {
-             label: item.formatted_address,
-             value: item.formatted_address
-             };
-             }));
-             });
-             }
-             });*/
-        });
-
-        // Prevent enter key
-        $('#dln_address').keypress(function (event) {
-            if (event.keyCode == 13) {
-                event.preventDefault();
-            }
-        });
-
-        /*$('#findbutton').click(function (e) {
-         var address = $('#' + field_address).val();
-         geocoder.geocode({ 'address': address }, function (results, status) {
-         if (status == google.maps.GeocoderStatus.OK) {
-         map.setCenter(results[0].geometry.location);
-         marker.setPosition(results[0].geometry.location);
-         $(latval).val(marker.getPosition().lat());
-         $(longval).val(marker.getPosition().lng());
-         } else {
-         alert("Geocode was not successful for the following reason: " + status);
-         }
-         });
-         e.preventDefault();
-         });*/
-
-        //Add listener to marker for reverse geocoding
-        google.maps.event.addListener(marker, 'drag', function () {
-            loadAddressMarker();
-        });
-
+       var map_drag = new MapDrag();
     });
 
-}(jQuery));
+}(window.jQuery));
