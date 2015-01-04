2cf7a13c85b8fd1755a3acb31823537b193716e3
lenhatdinh@gmail.com <lenhatdinh@gmail.com>
2015-01-04 17:30:35 +0700

    add

---
+++ theme-october/assets/js/dln-edit.js
@@ -0,0 +1,36 @@
+(function ($) {
+	$(document).ready(function () {
+		$(".dln-slider-price").ionRangeSlider({
+			type: "double",
+		    min: 0.5,
+		    max: 20,
+		    from: 1,
+		    to: 10,
+		    step: 0.1,
+		    grid: true,
+		    postfix: " triệu",
+		    max_postfix: "+",
+		    values_separator: " → "
+		});
+		
+		$.fn.tooltip && $('[data-toggle="tooltip"]').tooltip();
+		
+		$('.dln-slider-area').ionRangeSlider({
+			type: "double",
+		    min: 10,
+		    max: 200,
+		    from: 10,
+		    to: 90,
+		    step: 1,
+		    grid: true,
+		    postfix: " m<sup>2</sup>",
+		    max_postfix: "+",
+		    values_separator: " → "
+		});
+		
+		window.SelectFx && $('select[data-init-plugin="cs-select"]').each(function() {
+            var el = $(this).get(0);
+            $(el).wrap('<div class="cs-wrapper"></div>'), new SelectFx(el)
+        })
+	});
+}(jQuery));
