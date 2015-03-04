a4f692f56443b68ba7f29a595f3ae3ceb19276af
lenhatdinh@gmail.com <lenhatdinh@gmail.com>
2015-03-04 21:51:28 +0700

    Revert "test"
    This reverts commit 4053c756cbeb52239d343958ea67bd342ebf71ee.
    
    Conflicts:
    	dln-hot-point/.idea/workspace.xml
    	dln-hot-point/client_backbone/assets/app/app.js
    	dln-hot-point/client_backbone/assets/app/helpers/user-helper.js
    	dln-hot-point/client_backbone/assets/app/routers/AppRouter.js
    	dln-hot-point/client_backbone/assets/app/tpl/LoginView.html
    	dln-hot-point/client_backbone/assets/app/tpl/ShellView.html
    	dln-hot-point/client_backbone/assets/app/views/HomeView.js
    	dln-hot-point/client_backbone/assets/app/views/LoginView.js
    	dln-hot-point/client_backbone/index.html

---
+++ angular-app/realapp/README.md
@@ -0,0 +1,7 @@
+# Dln real app
+
+A simplified version of the ngBoilerplate angular project template.
+
+***
+
+## Quick Start
---
+++ angular-app/realapp/src/app/app.js
@@ -0,0 +1,20 @@
+(function(app) {
+
+    app.config(function ($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise('/home');
+    });
+
+    app.run(function () {});
+
+    app.controller('AppController', function ($scope) {
+
+    });
+
+}(angular.module("dlnRealApp", [
+    'dlnRealApp.home',
+    'dlnRealApp.about',
+    'templates-app',
+    'templates-common',
+    'ui.router.state',
+    'ui.router',
+])));
