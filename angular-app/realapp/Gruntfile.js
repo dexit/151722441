71f3c4714c1b94aeb1d18e21c346b78408ce2f54
lenhatdinh@gmail.com <lenhatdinh@gmail.com>
2015-03-04 23:57:02 +0700

    add some files

--- angular-app/realapp/Gruntfile.js
+++ angular-app/realapp/Gruntfile.js
@@ -598,7 +598,7 @@ module.exports = function(grunt) {
     // The 'compile' task gets your app ready for deployment by concatenating and minifying your code.
     // Note - compile builds off of the build dir (look at concat:compile_js), so run grunt build before grunt compile
     grunt.registerTask('compile', [
-        'less:compile', 'copy:compile_assets', 'concat:compile_js', 'uglify', 'index:compile'
+        'less:compile', 'copy:compile_assets', 'concat:compile_js', 'index:compile'
     ]);
 
     // A utility function to get all app JavaScript sources.
