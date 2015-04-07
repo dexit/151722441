677d0f98d6106b89305affd62a14ea506e18a676
DinhLN <dinhln@nadia.bz>
2015-04-07 17:51:52 +0700

    add google news

---
+++ dln-october/dlnlab/classified/classes/RestGoogleNews.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace DLNLab\Classified\Classes;
+
+use Auth;
+use DB;
+use Input;
+use Response;
+use Request;
+use Redirect;
+use Validator;
+use Illuminate\Routing\Controller as BaseController;
+use October\Rain\Support\ValidationException;
+use DLNLab\Classified\Components\Account;
+
+require('HelperResponse.php');
+
+class RestGoogleNews extends BaseController {
+
+    public function getNews() {
+        $data = get();
+        $default = array (
+            'topic' => ''
+        );
+        extract(array_merge($default, $data));
+
+        $tp = '';
+        switch($topic) {
+            case 'giaitri':
+                $tp = 'e';
+                break;
+
+            case 'thegioi':
+                $tp = 'w';
+                break;
+
+            case 'vietnam':
+                $tp = 'n';
+                break;
+
+            case 'kinhdoanh':
+                $tp = 'b';
+                break;
+
+            case 'thethao':
+                $tp = 's';
+                break;
+        }
+
+
+    }
+
+}
